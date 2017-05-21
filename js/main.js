aed5884397ccc3a60565bfabb407ec9e67f03427
Helen Gladun <helen.gladun@gmail.com>
2017-05-21 21:21:34 +0300

    Added structure and functionality for JS converter

---
+++ js/main.js
@@ -0,0 +1,41 @@
+function customConverterToUah() {
+    // setting variables
+    var euros = parseInt(prompt('How many euros do you want to convert?', '100')),
+        dollars = parseInt(prompt('How many dollars do you want to convert?', '100')),
+        eurRate = 29.54,
+        usdRate = 26.35,
+        output = '';
+
+    // currency convertation
+    var EurToUah = mainConverter(euros, eurRate);
+    var UsdToUah = mainConverter(dollars, usdRate);
+    var OneEurToUsd = Number(eurRate/usdRate).toFixed(3);
+
+     if (euros && EurToUah) {
+        output = euros + ' euros are equal ' + EurToUah + ' grns, ';
+     }
+
+     if (dollars && UsdToUah) {
+         output = output.concat(dollars + ' dollars are equal ' + UsdToUah + ' grns, ');
+     }
+
+     if (OneEurToUsd) {
+         output = output.concat(' one euro is equal ' +  OneEurToUsd + 'dollars.');
+     }
+
+    //result
+    alert(output);
+}
+
+function mainConverter(currencyNumber, currencyRate) {
+    if (typeof currencyNumber === 'number' && typeof currencyNumber === 'number') {
+        return currencyNumber * Number(parseFloat(currencyRate).toFixed(2));
+    }
+    else {
+        return null;
+    }
+}
+
+document.querySelector('.push-to-convert').addEventListener( "click" , customConverterToUah);
+
+
